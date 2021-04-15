0b10b6c5ba2ac537ee7a29d1bde1bf173db87a30
roman.manko <romanmanco@gmail.com>
2021-04-15 17:12:15 +0300

    Подключение к БД. Отображение статей. Добавление функционала редактирования и удаления.

--- src/main/java/com/myZodiac/blogMyZodiac/controllers/BlogController.java
+++ src/main/java/com/myZodiac/blogMyZodiac/controllers/BlogController.java
@@ -46,11 +46,49 @@ public class BlogController {
 
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable(value = "id") long postId, Model model){
+        if(!postRepo.existsById(postId)){
+            return "redirect:/blog";
+        }
         Optional<Post> post = postRepo.findById(postId);
         ArrayList<Post> result = new ArrayList<>();
         post.ifPresent(result::add);
         model.addAttribute("post", result);
         return "blogDetails";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long postId, Model model){
+        if(!postRepo.existsById(postId)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepo.findById(postId);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blogEdit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long postId,
+                                 @RequestParam String title,
+                                 @RequestParam String anons,
+                                 @RequestParam String full_text,
+                                 Model model){
+        Post post = postRepo.findById(postId).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepo.save(post);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") long postId, Model model){
+        Post post = postRepo.findById(postId).orElseThrow();
+        postRepo.delete(post);
+
+        return "redirect:/blog";
     }
 
 }

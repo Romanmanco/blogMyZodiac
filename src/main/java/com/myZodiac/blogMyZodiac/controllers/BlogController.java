252beb5fb13357699aa2fc4246104177cf7d33f8
roman.manko <romanmanco@gmail.com>
2021-04-15 15:13:39 +0300

    Подключение к БД. Отображение статей. Добавление кнопок редактирования и удаления.

--- src/main/java/com/myZodiac/blogMyZodiac/controllers/BlogController.java
+++ src/main/java/com/myZodiac/blogMyZodiac/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Controller
@@ -38,6 +42,15 @@ public class BlogController {
         Post post = new Post(title, anons, full_text);
         postRepo.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long postId, Model model){
+        Optional<Post> post = postRepo.findById(postId);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blogDetails";
     }
 
 }
